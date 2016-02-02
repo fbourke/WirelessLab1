955b23f9dd9f969a539ac1ba880c7f0ed06166ea
Evan Dorsky <evan.dorsky@icloud.com>
2016-02-02 15:01:48 -0500

    Attempt at demodulating (doesn't work)

--- process_data.m
+++ process_data.m
@@ -1,15 +1,30 @@
 clear all
 close all
 
-load rx2
+load 'rxdata/rx2'
 
-datadown = data(1:1e3:end);
-DATADOWN2 = fft(datadown.^2);
+j = sqrt(-1);
 
-plot(abs(DATADOWN2))
+data = data';
+datalen = numel(data)
+seg = data(datalen/2:5*datalen/6);
+xs = 1:numel(seg);
 
-[peak_amp, peak_freq] = max(abs(DATADOWN2))
+SEG = fft(seg.^2);
 
-Fs = .25e6;
-w_m = peak_freq/2;
+figure(1)
+plot(abs(SEG))
 
+[peak_amp, peak_freq] = max(abs(SEG))
+
+% Fs = .25e6;
+f_m = peak_freq/2;
+
+comp = exp(j*f_m*xs);
+seg_demod = seg.*comp;
+
+figure(2)
+plot(real(seg))
+hold on
+plot(real(seg_demod))
+legend('Seg', 'Seg Demod')
