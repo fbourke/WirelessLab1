ea588a124662577e99fadd2f3d7aa8e753979b8b
fbourke <fbourke@users.noreply.github.com>
2016-02-02 11:54:19 -0500

    add data generaton script

---
+++ generate_data.m
@@ -0,0 +1,20 @@
+y = zeros(1,1e6);
+% y(1:10000) = 0;
+% y(1e5:2e5) = 1;
+% y(2e5:3e5) = -1;
+% y(3e5:4e5) = 1;
+% y(4e5:5e5) = -1;
+% y(5e5:6e5) = 1;
+% y(6e5:7e5) = -1;
+% y(7e5:8e5) = 1;
+% y(8e5:9e5) = -1;
+% y(9e5:10e5) = 1;
+
+x = 1:1e2;
+y = cos(x*pi);
+pw = 1e4;
+out = conv(upsample(y,pw),ones(1,pw));
+
+plot(out)
+
+write_usrp_data_file(out, 'data.dat')
