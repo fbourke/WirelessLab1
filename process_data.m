d1ea03359ac4671d7ed6a642b6335543f04a7d0f
Evan Dorsky <evan.dorsky@icloud.com>
2016-02-09 11:26:55 -0500

    Costas loop working!

--- process_data.m
+++ process_data.m
@@ -11,31 +11,50 @@ function res = process_data()
     xs = 1:length(seg);
 
     SEG = fft(seg.^2);
-
-    figure(1)
-    plot(abs(SEG))
+    % figure(1)
+    % plot(abs(SEG))
 
     [peak_amp, peak_bin] = max(abs(SEG));
-
-    scaling = 2*pi;
 
     f_m = peak_bin/length(xs)*2*pi; % convert to rads/sample
 
     comp = exp(-j*f_m/2*xs);
     seg_demod = seg.*comp;
 
-    mag = 3e-3
-    thresh = .5*mag
+    % costas
 
-    filtered = schmitt(real(seg_demod), thresh, -thresh);
-    sscale = .017;
+    B = 4;
+    Phi = zeros(size(seg_demod));
+    for i = 1:length(seg_demod)-1
+        v = seg_demod(i);
+        e = -real(v)*imag(v);
+        Phi(i+1) = Phi(i) + B*e;
+    end
+
+    size(seg_demod)
+    size(Phi)
+    seg_costas = seg_demod.*exp(j*Phi);
+    size(seg_costas)
 
     figure(2)
     clf
-    plot(real(seg))
-    hold on
     plot(real(seg_demod))
-    plot(filtered*sscale-sscale/2, 'r', 'linewidth', 2)
+    hold on
+    plot(real(seg_costas))
+    legend('F-offset demodulated', 'Costas Demodulated')
+    
+    % schmitt
+    % mag = 3e-3
+    % thresh = .5*mag
+    % filtered = schmitt(real(seg_demod), thresh, -thresh);
+    % sscale = .017;
 
-    legend('Seg', 'Seg Demod', 'Schmitt Data')
+    % figure(3)
+    % clf
+    % plot(real(seg))
+    % hold on
+    % plot(real(seg_demod))
+    % plot(filtered*sscale-sscale/2, 'r', 'linewidth', 2)
+
+    % legend('Seg', 'Seg Demod', 'Schmitt Data')
 end
