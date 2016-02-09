97ccbfb50aa798aed31199e1f798054c8b235afc
fbourke <fbourke@users.noreply.github.com>
2016-02-09 11:40:12 -0500

    add working decode

--- generate_data.m
+++ generate_data.m
@@ -1,31 +1,35 @@
-% input = 'Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! '
+input = 'Hello, World!'
 y = 1*ones(1,96); % generates vector of leading 100 values of 0.5
-% binary = (dec2bin(input,8)); 
-% binary = binary(:)'-'0'; %moves 'binary' to a single row
-% binary = (binary - 0.5).*2; % binary is now +1 and -1
-binary = repmat([1,-1,-1,-1,-1,-1,-1,-1],1,200);
-y = horzcat(y,binary);
+binary = (dec2bin(input,8)); 
+binary = reshape(binary',1,[]); %moves 'binary' to a single row
+binary = binary - '0'; %converts binary from string to number
+binary = (binary - 0.5).*2; % binary is now +1 and -1
+binary = repmat(binary,1,2);
+y = horzcat(y,[1,-1,-1,-1,-1,-1,-1,-1],[1,-1,-1,-1,-1,-1,-1,-1],binary);
 
 pw = 30; % pulse width
 out = conv(upsample(y,pw),ones(1,pw));
 sample_freq = 1e6; % minimum .25 megasamples/sec
 datarate = sample_freq/pw
 
-x = 1:1e3;
-y = cos(x*pi); % generates vector of alternating 1 and -1
-out2 = conv(upsample(y,pw),ones(1,pw));
+% x = 1:1e3;
+% y = cos(x*pi); % generates vector of alternating 1 and -1
+% out2 = conv(upsample(y,pw),ones(1,pw));
 
-plot(out)
-hold all
+% plot(out)
+% hold all
 % plot(out2,'linewidth',2)
-legend('current','working');
+% legend('current','working');
 
 write_usrp_data_file(out, 'data.dat')
 
 %%
-A = y;
-B = reshape(A,[],8);
-B = (B+1)./2
-C = bi2de(B);
-D = char(C);
-E = reshape(D,1,[])
+% for offset = 1:20 
+
+%     A = horzcat(y((8*11+offset+1):end), ones(1,offset));
+    A = (y(1:end)+1)./2
+    B = reshape(A,8,[])'
+    C = bi2de(fliplr(B));
+    D = char(C);
+    E = reshape(D,1,[])
+% end
