f882daed80ee5a7086e58f794f27ce8da88838af
Evan Dorsky <evan.dorsky@icloud.com>
2016-02-19 11:57:00 -0500

    Add channel estimation stuff

--- lab2/process.m
+++ lab2/process.m
@@ -11,12 +11,28 @@ tr2 = 15140:20330;
 td = 17;
 
 h11x = y1(tr1)./x1(tr1-td);
-h21x = y2(tr1)./x2(tr1-td);
+h21x = y2(tr1)./x1(tr1-td);
 
 h11 = mean(h11x(~isinf(h11x)));
 h21 = mean(h21x(~isinf(h21x)));
 
+h12x = y1(tr2)./x2(tr2-td);
+h22x = y2(tr2)./x2(tr2-td);
+
+h12 = mean(h12x(~isinf(h12x)));
+h22 = mean(h22x(~isinf(h22x)));
+
+H = [h11 h12;
+     h21 h22]
+
+w = inv(H')*[1; 0];
+
+xhat = y1*w';
+
 figure(1)
+clf
+plot(real(xhat))
 hold on
-plot(real(h11x))
-plot(imag(h11x))
+plot(imag(xhat))
+
+legend('Real 1', 'Real 2', 'Imag 1', 'Imag 2')
