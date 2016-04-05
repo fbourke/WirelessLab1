b77f3de9f77d0740973a0e187e063e0598689e18
Evan Dorsky <evan.dorsky@icloud.com>
2016-04-05 11:39:35 -0400

    Lab 3 infrastructure

---
+++ lab3/read_data.m
@@ -0,0 +1,18 @@
+clear all
+close all
+
+data = read_usrp_data_file('rx/usrp_samples.dat');
+
+dfactor = 1
+
+figure(1)
+plot(abs(data(1:dfactor:end)))
+title('Our beautiful data')
+xlabel('"Sample"')
+ylabel('Amplitude')
+
+figure(2)
+plot(data(1:dfactor:end))
+title('Our beautiful data')
+xlabel('Real')
+ylabel('Imag')
